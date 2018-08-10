fa4e1acadbde61402d46e8af186a8034218c8ad2
slipper <slipper@0ops.net>
2018-08-10 07:53:23 -0700

    do not always use tls; do not set shaper rules

--- service/ejabberd.sh
+++ service/ejabberd.sh
@@ -2,6 +2,7 @@
 set -e
 docker inspect ejabberd >/dev/null && docker kill ejabberd
 # FIXME do not expose manage console in production
-docker run --rm --name ejabberd -d -p 5222:5222 -p 5280:5280 -v $(pwd)/ejabberd.yml:/home/ejabberd/conf/ejabberd.yml ejabberd/ecs
-sleep 3
+# docker run --rm --name ejabberd -d -v $(pwd)/ejabberd.yml:/home/ejabberd/conf/ejabberd.yml ejabberd/ecs
+docker run --rm --name ejabberd -d -p 5222:5222 -v $(pwd)/ejabberd.yml:/home/ejabberd/conf/ejabberd.yml ejabberd/ecs
+sleep 5
 docker exec -it ejabberd bin/ejabberdctl register admin ooo.vchat passw0rd
